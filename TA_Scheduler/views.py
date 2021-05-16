3a4933734bc21dbcf6684b627f541659939a4580
Primeosie24 <ugbodaga@uwm.edu>
2021-05-16 16:02:58 -0500

    peter_more_work

--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from TA_Scheduler.models import Account, Course, PersonalInfo, LabSection
-
+from TA_Scheduler.models import Account, Course, PersonalInfo, Lab
 from django.contrib import messages, auth
 from django.contrib.auth import authenticate
 
@@ -96,7 +95,6 @@ class CoursesView(View):
                                                "username": request.session.get("username")})
 
 
-
 # not yet implemented
 class EditView(View):
     def get(self, request):
@@ -119,13 +117,13 @@ class InstructorToCourse(View):
     def post(self, request):
         instructor_name = request.POST["instructor_name"]
         course_name = request.POST["course_name"]
-        instructor = Account.objects.filter(name=instructor_name)
         course = Course.objects.filter(name=course_name)
-        course.instructor = instructor
+        course.update(instructor_name=instructor_name)
 
         return render(request, 'AdminP.html', {"accounts": Account.objects.all(),
                                                "courses": Course.objects.all(),
                                                "username": request.session.get("username")})
+
 
 class TaToCourse(View):
     def get(self, request):
@@ -187,35 +185,18 @@ def Personal_Info_Instructor(request):
 
 
 # creating lab sections for the courses
-def Create_Section(request):
-    if request.method == 'GET':
-        # admin - landing page loading
-        course = Course.objects.all()
-        TaList = Account.objects.filter(status='Ta')
-        InstructorList = Account.objects.filter(status='Instructor')
-        return render(request, 'lab.html',
-                      {'TaList': TaList, 'InstructorList': InstructorList, 'CourseList': course})
-    if request.method == 'POST':
-        # admin - create sections
-        section_course = request.POST['Course Pick']
-        course_obj = Course.objects.get(id=section_course)
-        section_instructor = request.POST['taInstructor']
-        if section_instructor == 'tbd':
-            instructor_obj = "n"
-        else:
-            instructor_obj = Account.objects.get(id=section_instructor)
-        section_number = request.POST['sectionNumber']
-        section_meeting_time = request.POST['sectionMeet']
-        section_meeting_location = request.POST['sectionLocation']
-        if type(instructor_obj) == str:
-            # this stores a null in the section.instructorOrTa field
-            section = LabSection.objects.create(course=course_obj,
-                                                number=section_number, meeting_time=section_meeting_time,
-                                                meeting_location=section_meeting_location)
-        else:
-            section = LabSection.objects.create(course=course_obj, instructorOrTa=instructor_obj,
-                                                number=section_number, meeting_time=section_meeting_time,
-                                                meeting_location=section_meeting_location)
+class LabsView(View):
+    def get(self, request):
+        if request.session.get("username"):
+            return render(request, 'lab.html')
+        return render(request, "Login.html")
 
-        messages.success(request, 'SECTION SUCCESSFULLY CREATED')
-        return redirect('Create_Section')
+    def post(self, request):
+        lab = Lab.objects.create(lab_name=request.POST["lab_name"],
+                                 lab_id=request.POST["lab_ID"],
+                                 course=request.POST["course"])
+
+        messages.success(request, 'Lab has been Created')
+        return render(request, 'Instructor.html', {"accounts": Account.objects.all(),
+                                                   "lab": Lab.objects.all(),
+                                                   "username": request.session.get("username")})
